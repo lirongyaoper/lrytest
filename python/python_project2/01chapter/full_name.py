245b3b491f735bddf456a5ef8d5ecbdc0197b7cc
lirongyaoper <lirongyaoper@163.com>
2023-02-13 22:27:55 +0800

    202302130001

---
+++ python/python_project2/01chapter/full_name.py
@@ -0,0 +1,10 @@
+first_name = "li"
+last_name = "rongyao"
+full_name  = f"{first_name}{last_name}"
+print(full_name)
+message= f"hellow,{full_name}"
+print(message)
+
+print("python")
+print("\tpython")
+print("languages:\npython\nC\nHavaScript")
--- python/python编程-从入门到实践-2version/chapter_02/full_name.py
+++ python/python编程-从入门到实践-2version/chapter_02/full_name.py
@@ -3,3 +3,4 @@ last_name = "lovelace"
 full_name = f"{first_name} {last_name}"
 message = f"Hello, {full_name.title()}!"
 print(message)
+import this
