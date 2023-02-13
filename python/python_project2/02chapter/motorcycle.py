91a0f21896c8f1a5b41785e3ccae5a1f46b7789e
lirongyaoper <lirongyaoper@163.com>
2023-02-13 22:55:33 +0800

    20230213002

---
+++ python/python_project2/02chapter/motorcycle.py
@@ -0,0 +1,12 @@
+motorcycles = ['honda', 'yamaha', 'suzuki', 'ducati'] 
+print(motorcycles)
+motorcycles.insert(1,"haojuemotuo")
+print(motorcycles)
+del motorcycles[1]
+print(motorcycles)
+print("###########################################################")
+motorcycles2 = ['honda', 'yamaha', 'suzuki'] 
+print(motorcycles2)
+poped_motorcycles2= motorcycles2.pop()
+print(motorcycles2)
+print(poped_motorcycles2)
