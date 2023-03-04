a4984c83cfbde99a704fa2d65793760eb2256096
lirongyaoper <lirongyaoper@163.com>
2023-03-04 23:30:54 +0800

    20230304001

--- python/lry_python_projects/LryAlienInvasion/alien_invasion_lry.py
+++ python/lry_python_projects/LryAlienInvasion/alien_invasion_lry.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settiongs_lry import Settings
+from ship_lry import Ship
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self) :
@@ -10,20 +11,40 @@ class AlienInvasion:
         self.settings = Settings()
         self.screen = pygame.display.set_mode((self.settings.screen_width,self.settings.screen_height))
         pygame.display.set_caption("外星人入侵大战")
+        self.ship   = Ship(self)
 
 
 
     def run_game(self):
         """开始游戏的主循环"""
         while True:
-            #监视键盘和鼠标事件
-            for even in pygame.event.get():
-                if even.type == pygame.QUIT:
-                    sys.exit()
-            #每次循环都重新绘制屏幕
-            self.screen.fill(self.settings.bg_color)
-            #让最近绘制的屏幕可见
-            pygame.display.flip()
+            self._check_events()
+            self.ship.update()
+            self._update_screen()
+
+    def _check_events(self):
+        #监视键盘和鼠标事件
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = True
+                elif event.key == pygame.K_LEFT:
+                    self.ship.moving_left = True
+                    #self.ship.rect.x += 1
+            elif event.type == pygame.KEYUP:
+                if event.key == pygame.K_RIGHT:
+                    self.ship.moving_right = False
+                elif event.key ==  pygame.K_LEFT:
+                    self.ship.moving_left = False
+
+    def _update_screen(self):
+        #每次循环都重新绘制屏幕
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+        #让最近绘制的屏幕可见
+        pygame.display.flip()
 
 if __name__ == '__main__':
     ##创建游戏实例并运行游戏
--- python/lry_python_projects/LryAlienInvasion/ship_lry.py
+++ python/lry_python_projects/LryAlienInvasion/ship_lry.py
@@ -6,10 +6,19 @@ class Ship:
         self.screen = ai_name.screen
         self.screen_rect = ai_name.screen.get_rect()
         #加载飞船图像并获取其外接矩形
-        self.image = pygame.image.load('images/ship.bmp')
+        self.image = pygame.image.load('E:/lrytest/python/lry_python_projects/LryAlienInvasion/images/ship.bmp')
         self.rect = self.image.get_rect()
         #对于每艘新飞船，都将其放在屏幕地步的中央
         self.rect.midbottom = self.screen_rect.midbottom
+
+        #移动标志
+        self.moving_right = False
+        self.moving_left = False
+    def update(self):
+        if self.moving_right:
+            self.rect.x += 1
+        if self.moving_left:
+            self.rect.x -= 1
     def blitme(self):
         """在指定位置绘制飞船"""
         self.screen.blit(self.image,self.rect)
