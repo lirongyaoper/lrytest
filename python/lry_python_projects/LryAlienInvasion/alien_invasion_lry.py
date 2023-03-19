8b338b015326688a422d94e7be3630ef4fdabeef
lirongyaoper <lirongyaoper@163.com>
2023-03-19 22:59:48 +0800

    200230319001

--- python/lry_python_projects/LryAlienInvasion/alien_invasion_lry.py
+++ python/lry_python_projects/LryAlienInvasion/alien_invasion_lry.py
@@ -2,6 +2,8 @@ import sys
 import pygame
 from settiongs_lry import Settings
 from ship_lry import Ship
+from bullet_lry import Bullet
+
 class AlienInvasion:
     """管理游戏资源和行为的类"""
     def __init__(self) :
@@ -14,14 +16,16 @@ class AlienInvasion:
         self.settings.screen_height = self.screen.get_rect().height      
         pygame.display.set_caption("外星人入侵大战")
         self.ship   = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
 
 
     def run_game(self):
-        """开始游戏的主循环"""
+        """开始游戏,，的主循环"""
         while True:
             self._check_events()
             self.ship.update()
+            self.bullets.update()
             self._update_screen()
 
     def _check_events(self):
@@ -41,16 +45,24 @@ class AlienInvasion:
             self.ship.moving_left = True  
         elif event.key == pygame.K_q:
             sys.exit()    
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
             self.ship.moving_right = False
         elif event.key ==  pygame.K_LEFT:
-            self.ship.moving_left = False               
+            self.ship.moving_left = False    
+    def _fire_bullet(self):
+        #"""创建一颗子弹，并将其加入编组bullets中"""  
+        new_bullet= Bullet(self)
+        self.bullets.add(new_bullet)       
 
     def _update_screen(self):
         #每次循环都重新绘制屏幕
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
+        for bullet in self.bullets.sprites():
+            bullet.draw_bullet()
         #让最近绘制的屏幕可见
         pygame.display.flip()
 
